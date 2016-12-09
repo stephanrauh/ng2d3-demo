4f1ea225ac7d29ecca94dca58228811126911fd6
stephan <webmaster1@beyondjava.de>
2016-12-09 14:49:34 +0100

    initial commit

---
+++ src/app/linechart/linechart.component.ts
@@ -0,0 +1,44 @@
+import {Component} from "@angular/core";
+import {single, multi} from "../data";
+import {NG2D3Module} from 'ng2d3';
+import d3 from '../d3';
+
+@Component({
+  selector: 'linechart',
+  templateUrl: './linechart.component.html',
+  styleUrls: ['./linechart.component.css']
+})
+export class LinechartComponent {
+  single: any[];
+  multi: any[];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+  curve = d3.shape.curveStep;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // pie
+  showLabels = true;
+  explodeSlices = false;
+  doughnut = false;
+
+  // line, area
+  autoScale = true;
+
+  constructor() {
+    Object.assign(this, {single, multi})
+  }
+
+}
